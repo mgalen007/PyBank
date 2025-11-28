8246ec264a5c99ff818783e96eae533a09808324
unknown <hyllusx@gmail.com>
2025-11-28 07:20:52 +0200

    Added a simple login page

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-import requests
+from flask import Flask, render_template, request
 import time
 
 
@@ -9,8 +8,21 @@ current_year = time.localtime().tm_year
 host = '127.0.0.1'
 port = 8080
 
-@app.route('/home')
-def home():
-    return render_template('index.html', username='Admin', year=current_year)
+@app.route('/')
+def auth():
+    return render_template('login.html')
 
-app.run(host, port)
+@app.route('/home', methods=['POST'])
+def home():
+    if request.method == 'POST': 
+        user = request.form['username']
+        return render_template('index.html', username=user, year=current_year)
+    
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host=host, port=port)
+
